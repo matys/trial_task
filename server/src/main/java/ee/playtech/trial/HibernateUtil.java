98d5bcc20fb8dd53821c79371ff1d932ff382acb
matys <maatttyys@gmail.com>
2014-03-26 15:28:21 +0100

    Hibernate added

--- server/src/main/java/ee/playtech/trial/HibernateUtil.java
+++ server/src/main/java/ee/playtech/trial/HibernateUtil.java
@@ -1,13 +1,13 @@
 package ee.playtech.trial;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory;
 	static {
 		try {
-			sessionFactory = new Configuration().configure()
+			sessionFactory = new AnnotationConfiguration().configure()
 					.buildSessionFactory();
 		} catch (Throwable ex) {
 			System.err.println("Initial SessionFactory creation failed." + ex);
--- server/src/main/java/ee/playtech/trial/server/model/entity/Player.java
+++ server/src/main/java/ee/playtech/trial/server/model/entity/Player.java
@@ -3,8 +3,12 @@ package ee.playtech.trial.server.model.entity;
 import java.math.BigDecimal;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "PLAYER")
 public class Player extends GenericEntity {
 
 	/**
